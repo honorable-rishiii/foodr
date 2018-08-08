11e003ca5706c9040b1dbb91562f13297e6f63e1
honorable-rishiii <39996762+honorable-rishiii@users.noreply.github.com>
2018-08-08 21:23:11 +0530

    Add files via upload

--- backup/login.php
+++ backup/login.php
@@ -1,47 +1,63 @@
-
 <?php
+// Include config file
 require_once 'config.php';
  
+// Define variables and initialize with empty values
 $username = $password = "";
 $username_err = $password_err = "";
  
+// Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
  
+    // Check if username is empty
     if(empty(trim($_POST["username"]))){
         $username_err = 'Please enter username.';
     } else{
         $username = trim($_POST["username"]);
     }
     
+    // Check if password is empty
     if(empty(trim($_POST['password']))){
         $password_err = 'Please enter your password.';
     } else{
         $password = trim($_POST['password']);
     }
     
+    // Validate credentials
     if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
         $sql = "SELECT username, password FROM users WHERE username = ?";
         
         if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_username);
             
+            // Set parameters
             $param_username = $username;
             
+            // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
+                // Store result
                 mysqli_stmt_store_result($stmt);
                 
-                if(mysqli_stmt_num_rows($stmt) == 1){                  
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
                     mysqli_stmt_bind_result($stmt, $username, $hashed_password);
                     if(mysqli_stmt_fetch($stmt)){
                         if(password_verify($password, $hashed_password)){
+                            /* Password is correct, so start a new session and
+                            save the username to the session */
                             session_start();
                             $_SESSION['username'] = $username;      
                             header("location: index.php");
                         } else{
+                            // Display an error message if password is not valid
                             $password_err = 'The password you entered was not valid.';
                         }
                     }
                 } else{
+                    // Display an error message if username doesn't exist
                     $username_err = 'No account found with that username.';
                 }
             } else{
@@ -49,9 +65,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             }
         }
         
+        // Close statement
         mysqli_stmt_close($stmt);
     }
     
+    // Close connection
     mysqli_close($link);
 }
 ?>
@@ -61,25 +79,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
-	<script type="text/javascript" src="particles.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-	<link rel="stylesheet" href="login.css" type="text/css" media=screen>
     <style type="text/css">
-        body{ font: 14px sans-serif;
-		 }
-        .wrapper{ width: 350px; padding: 20px; background color: #777;  }
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
     </style>
 </head>
 <body>
-	<div id = "particles-js></div>
-	<script type ="text/javascript" src= "particles.js"></script>
-	<script type ="text/javascript" src= "app.js"></script>
-
-
-
-
-    <div class="container">
-    <div class="login">
+    <div class="wrapper">
         <h2>Login</h2>
         <p>Please fill in your credentials to login.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
@@ -99,6 +106,5 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
         </form>
     </div>    
-</div>  
 </body>
 </html>
--- index.php
+++ index.php
@@ -10,14 +10,79 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
 
 
 ?>
- 
+
+
+
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Welcome</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">		
+<html>
+  <head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <title>Foodr</title>
+    <style>
+	.header {
+  overflow: hidden;
+  background-color: #f1f1f1;
+  padding: 20px 10px;
+   background: #555;
+  color: #f1f1f1;
+  position: inherit;
+  top: 10px;
+  left: 25%;
+  z-index: 500;
+  background-color: grey;
+  padding: 8px;
+  border: 1px solid #999;
+  text-align: center;
+  font-family: 'Roboto','sans-serif';
+  line-height: 20px;
+  padding-left: 55px;
+}
+
+/* Style the header links */
+.header a {
+  float: left;
+  color: white;
+  text-align: center;
+  padding: 12px;
+  text-decoration: none;
+  font-size: 18px; 
+  line-height: 25px;
+  border-radius: 4px;
+  font-family: 'Roboto','sans-serif';
+}
+
+/* Style the logo link (notice that we set the same value of line-height and font-size to prevent the header to increase when the font gets bigger */
+.header a.logo {
+  font-size: 25px;
+  font-weight: bold;
+}
+
+/* Change the background color on mouse-over */
+
+
+/* Style the active/current link*/
+.header a.active {
+  background-color: dodgerblue;
+  color: white;
+}
+
+/* Float the link section to the right */
+.header-right {
+  float: right;
+}
+
+/* Add media queries for responsiveness - when the screen is 500px wide or less, stack the links on top of each other */ 
+@media screen and (max-width: 500px) {
+  .header a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+  .header-right {
+    float: none;
+  }
+}
     #panel {
    background: #555;
   color: #f1f1f1;
@@ -43,23 +108,158 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
         margin: 0;
         padding: 0;
       }
-    
     </style>
-</head>
-<body>
-  <div class="header" id="panel">
-  <h2><center>Foodr</center></h2>
-  <?php echo htmlspecialchars($_SESSION['username']); ?>
-  <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+  </head>
+  <body>
+
+ <div class="header">
+ 
+ 
+ <a href="#default" class="logo">Foodr</a>
+ <div class="header-right">
+   <a  href="#home">
+      <?php echo htmlspecialchars($_SESSION['username']); ?>
+     </a>
+   
+     
+     
+   <a href="logout.php">Sign out</a>
+ </div>
 </div>
 
+    <div id="map"></div>
+    <script>
+ 
+      function initMap() {
+        var fab = {lat: 6.870849, lng: 79.886745};
+        var sch = {lat: 6.870386, lng: 79.888468};
+        var Ps = {lat: 6.868895699999999, lng: 79.8873039};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 14,
+          center: fab
+        });
+ 
+        var contentStringfab = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading"><center>The Fab</h1>'+
+			'<div id="wowslider-container0">'+
+	'<div class="ws_images"><ul>'+
+		'<li><img src="Slide show/data0/images/fabgroup2.jpg" alt="Since 1979" title="Since 1979" id="wows0_0"/></li>'+
+		'<li><img src="Slide show/data0/images/images_1.jpg" alt="Delicious cakes" title="Delicious cakes" id="wows0_1"/></li>'+
+		'<li><img src="Slide show/data0/images/formatfactorypastrycopy.jpg" alt="bootstrap slider" title="Short eats" id="wows0_2"/></a></li>'+
+		'<li><img src="Slide show/data0/images/06050001.jpg" alt="Fresh foods" title="Fresh foods" id="wows0_3"/></li>'+
+	'</ul></div>'+
+	'<div class="ws_bullets"><div>'+
+	'	<a href="#" title="Since 1979"><span><img src="Slide show/data0/tooltips/fabgroup2.jpg" alt="Since 1979"/>1</span></a>'+
+	'	<a href="#" title="Delicious cakes"><span><img src="Slide show/data0/tooltips/images_1.jpg" alt="Delicious cakes"/>2</span></a>'+
+	'<a href="#" title="Short eats"><span><img src="Slide show/data0/tooltips/formatfactorypastrycopy.jpg" alt="Short eats"/>3</span></a>'+
+	'	<a href="#" title="Fresh foods"><span><img src="Slide show/data0/tooltips/06050001.jpg" alt="Fresh foods"/>4</span></a>'+
+	'</div></div><div class="ws_script" style="position:absolute;left:-99%"></div>'+
+	'<div class="ws_shadow"></div>'+
+	'</div>'+
+	
+            
+'<p><center><button class="button button2"><div id="left">Pasta</div><div id="right">Rs.85/=</div></button>'+
+'<button class="button button2"><div id="left">Cake</div><div id="right">Rs.500/=</button>'+
+'<button class="button button2"><div id="left">Rolls</div><div id="right">Rs.50/=</div></button>'+
+'<button class="button button2"><div id="left">Pastries</div><div id="right">Rs.45/=</div></button>'+
+            '</div>'+
+            '</div>';
+           
+        var contentStringsch = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Shanthi Cream House</h1>'+
+            '<div id="bodyContent">'+
+            
+'<p><center><button class="button button2"><div id="left">Pasta</div><div id="right">Rs.85/=</div></button>'+
+'<button class="button button2"><div id="left">Cake</div><div id="right">Rs.250/=</button>'+
+'<button class="button button2"><div id="left">Rolls</div><div id="right">Rs.50/=</div></button>'+
+'<button class="button button2"><div id="left">Pastries</div><div id="right">Rs.45/=</div></button>'+
+            '<button class="button button2"><div id="left">Chicken Bun</div><div id="right">Rs.70/=</div></button>'+
+            '</div>'+
+            '</div>';
+			
+		var contentStringPs = '<div id="content">'+
+              '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading"><center>Perera and Sons</h1>'+
+            '<div id="bodyContent">'+
+              '<p><center><button class="button button2"><div id="left">Pasta</div><div id="right">Rs.85/=</div></button>'+
+'<button class="button button2"><div id="left">Cake</div><div id="right">Rs.250/=</button>'+
+'<button class="button button2"><div id="left">Rolls</div><div id="right">Rs.50/=</div></button>'+
+'<button class="button button2"><div id="left">Pastries</div><div id="right">Rs.45/=</div></button>'+
+            '<button class="button button2"><div id="left">Chicken Bun</di v><div id="right">Rs.70/=</div></button>'+
+            '</div>'+
+            '</div>';
+ 
+        var infowindowfab = new google.maps.InfoWindow({
+          content: contentStringfab
+        });
+ 
+        var infowindowsch = new google.maps.InfoWindow({
+          content: contentStringsch
+        });
+		
+         var infowindowPs =new google.maps.InfoWindow({
+          content: contentStringPs
+         });
+ 
+        var markerfab = new google.maps.Marker({
+          position: fab,
+          map: map,
+          title: 'fab'
+        });
+        markerfab.addListener('click', function() {
+          infowindowfab.open(map, markerfab);
+        });
+        var markersch = new google.maps.Marker({
+          position: sch,
+          map: map,
+          title: 'shanthi cream house'
+        });
+        markersch.addListener('click', function() {
+          infowindowsch.open(map, markersch);
+        });
+		
+          var  markerPs = new google.maps.Marker({
+            position: Ps,
+            map: map,
+            title: 'Perera & Sons',
+            icon: 'Pictures/Rest.png'
+           });
+          markerPs.addListener('click', function() {
+              infowindowPs.open(map, markerPs);
+          });
+		  
+		var slideIndex = 1;
+			showDivs(slideIndex);
+		}
+				
+		function plusDivs(n) {
+		  showDivs(slideIndex += n);
+		}
+		
+		
+		function showDivs(n) {
+		  var i;
+		  var x = document.getElementsByClassName("mySlides");
+		  if (n > x.length) {slideIndex = 1}    
+		  if (n < 1) {slideIndex = x.length}
+		  for (i = 0; i < x.length; i++) {
+			 x[i].style.display = "none";  
+		  }
+		  x[slideIndex-1].style.display = "block";  
+		}
 
-    <div class="page-header">
-        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION['username']); ?></b>. Welcome to our site.</h1>
-        <p><?php $sql = "SELECT * FROM users WHERE `username`='".$_SESSION['username']."'";
-				 ?></p>
-				<a href ="mapsacc.html">continue</a>
-    </div>
 
-</body>
+    </script>
+    <script async defer
+   src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDpJaIbH1Nu6_DkmqPAmKZwgsANAKGb2fg&callback=initMap">
+    </script>
+	
+    <script type="text/javascript" src="Slide show/engine0/wowslider.js"></script>
+	<script type="text/javascript" src="Slide show/engine0/script.js"></script>
+  </body>
 </html>
